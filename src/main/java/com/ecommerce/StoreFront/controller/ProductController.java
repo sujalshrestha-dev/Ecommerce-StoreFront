09faff11aab631e4d1b9ea79bdd58cff1e6f728a
Sujal Shrestha <shresthasujal2021@gmail.com>
2026-05-30 11:32:08 +0545

    ProductController Error

--- src/main/java/com/ecommerce/StoreFront/controller/ProductController.java
+++ src/main/java/com/ecommerce/StoreFront/controller/ProductController.java
@@ -1,14 +1,29 @@
 package com.ecommerce.StoreFront.controller;
 
+import com.ecommerce.StoreFront.model.Product;
 import com.ecommerce.StoreFront.service.CategoryService;
+import com.ecommerce.StoreFront.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/products")
 public class ProductController {
 
-    @Autowired
-    private CategoryService categoryService;
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+
 }
