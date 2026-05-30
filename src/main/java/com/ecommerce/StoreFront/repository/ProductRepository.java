1c36b2f6f4784d10184b63a20757ea4d969c7099
Sujal Shrestha <shresthasujal2021@gmail.com>
2026-05-30 11:23:59 +0545

    service Layer completed

--- src/main/java/com/ecommerce/StoreFront/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/StoreFront/repository/ProductRepository.java
@@ -4,7 +4,9 @@ import com.ecommerce.StoreFront.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    List<Product> findByCategoryId(Long categoryId);
 }
--- src/main/java/com/ecommerce/StoreFront/service/ProductService.java
+++ src/main/java/com/ecommerce/StoreFront/service/ProductService.java
@@ -1,4 +1,27 @@
 package com.ecommerce.StoreFront.service;
 
+import com.ecommerce.StoreFront.model.Category;
+import com.ecommerce.StoreFront.model.Product;
+import com.ecommerce.StoreFront.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public List<Product> getAllProductsByCategory(Long categoryId) {
+        return productRepository.findByCategoryId(categoryId);
+    }
 }
